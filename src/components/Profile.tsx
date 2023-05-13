cbc53271b3035ec8791536e0a54e37d24f6c4cd3
Khaif Mohamed <khaifmohamededava@gmail.com>
2023-05-13 08:29:14 +0530

    second commit

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -2,12 +2,13 @@
 
 import {useSession} from 'next-auth/react'
 import {signOut} from 'next-auth/react'
+import Image from 'next/image'
 
 export default function Profile() {
     const {data: session} = useSession()
   return (
     <div className='flex flex-col items-center justify-center text-center'>
-        <img src={session?.user?.image!} 
+        <Image src={session?.user?.image!} 
         className='rounded-full w-14 h-14 cursor-pointer '
         onClick={() => signOut()}
         alt="profile-pic" />
